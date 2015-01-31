92af68adaade9c8f514cbc03f6560fbddb2ceb78
Yusuke Nakamura <muken.the.vfrmaniac@gmail.com>
2015-01-31 12:23:19 +0900

    Fix a compilation on Linux.

--- AlignedMemory.cpp
+++ AlignedMemory.cpp
@@ -18,6 +18,7 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *****************************************************************************/
 
+#include <cstddef>
 #include <cassert>
 #include <cstdlib>
 
